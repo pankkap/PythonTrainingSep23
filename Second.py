e73f955fa56acff0cf587535f7ae51f2ce0cd839
pankkap <pankkap@gmail.com>
2023-09-25 15:15:38 +0530

    day-1

---
+++ Second.py
@@ -0,0 +1,79 @@
+# Basic of Python
+# Comments
+
+# this is single line comment 
+# shortcut in vscode ctrl + / for comment and uncomment
+
+''' this is multiline comment
+this is multiline comment
+this is multiline comment
+'''
+print("hello")
+# {
+#     code block
+# }
+
+# Message string
+print("This is treated as message string")
+print('This is treated as message string')
+
+
+
+# variable in Python
+# int a = 10
+# float b = 12.5
+
+a = 10
+# a = "Pankaj" 
+b = 12.5
+
+print(type(a))
+print(type(b))
+
+c = a + b
+print(type(c))
+# print("Result of addition of two numbers: ", 5 + 10)
+print("Result of addition of two numbers: ", c)
+
+
+
+# Data types in Python
+# 1. number
+#      int
+#      float
+# 2. string
+# 3. boolean
+# 4. Data Structure
+    # list
+    # tuple
+    # set
+    # dictionary
+
+
+
+fname = "Pankaj"
+lname = 'kapoor'
+address = '''Greater Noida
+Sector:Chi-III
+Pincode: 201306'''
+
+print(fname)
+print(lname)
+print(address)
+
+holiday = False
+wfo = True
+
+print(holiday)
+print(wfo)
+
+
+
+
+# iNPUT dATA 
+
+num1 = int( input("Enter a Number: "))
+num2 = int( input("Enter second Number: "))
+print(num1+num2)
+
+
