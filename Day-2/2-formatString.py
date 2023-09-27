a474bbc0e528385d9cabb44a094f8f30dfb26f0a
pankkap <pankkap@gmail.com>
2023-09-27 14:06:06 +0530

    changes

---
+++ Day-2/2-formatString.py
@@ -0,0 +1,9 @@
+msg = "welcome {} to Ingenuity Gaming"
+# name = input("Enter name: ")
+# print(msg.format(name))
+
+gameName = "Ginni"
+gameSize = 3
+gamePrice = 125
+gameMsg = "I want to play {} which is of {}mb having price ${:.2f} cent"
+print(gameMsg.format(gameName, gameSize, gamePrice))
---
+++ Day-2/4-ConditionlStr.py
@@ -0,0 +1,20 @@
+# conditional Structure
+# if 
+# if -else
+# if -elif
+
+x = 1
+if(x>5):
+    print("Number is greater than 5")
+    print("Hi this is a statement")
+elif(x==0):
+    print("Number is zero")
+else:
+    if x < 5:
+        print("Number is less than 5")
+print("Value of x is ",x)
+
+
+if(x==5):
+    pass
+
